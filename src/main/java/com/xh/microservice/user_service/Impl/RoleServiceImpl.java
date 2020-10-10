9eb1f89979be41b3a5752bd903af9f4e447820f8
thinhan12138 <han_thin@163.com>
2020-10-10 21:20:57 +0800

    2020/10/10，添加鉴权功能

--- src/main/java/com/xh/microservice/user_service/Impl/RoleServiceImpl.java
+++ src/main/java/com/xh/microservice/user_service/Impl/RoleServiceImpl.java
@@ -10,22 +10,22 @@ import com.xh.microservice.common.support.Query;
 import com.xh.microservice.common.utils.StringUtils;
 import com.xh.microservice.user_constants.UserConstants;
 import com.xh.microservice.user_entity.Role;
-import com.xh.microservice.user_entity.User;
+import com.xh.microservice.user_entity.RoleMenu;
 import com.xh.microservice.user_mapper.RoleMapper;
+import com.xh.microservice.user_mapper.RoleMenuMapper;
+import com.xh.microservice.user_pojo.MenuPojo;
 import com.xh.microservice.user_pojo.RoleMenuPojo;
 import com.xh.microservice.user_pojo.RolePojo;
+import com.xh.microservice.user_service.RoleMenuService;
 import com.xh.microservice.user_service.RoleService;
-import com.xh.microservice.user_vo.UserVo;
-import com.xh.microservice.user_vo.UserWrapper;
-import org.springframework.beans.BeanUtils;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 
 /**
  * @Author: x18266
@@ -34,6 +34,9 @@ import java.util.List;
  */
 @Service
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements RoleService {
+
+    @Autowired
+    private RoleMenuService roleMenuService;
 
     @Override
     public IPage<Role> listRole(String roleName, Query query) {
@@ -49,7 +52,7 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
 
     @Override
     @Transactional
-    public Role addRole(RolePojo rolePojo) {
+    public Role addRole(final RolePojo rolePojo) {
         final String roleName = rolePojo.getRoleName();
         final int exists = this.baseMapper.selectCount(new QueryWrapper<Role>().lambda()
                 .eq(Role::getRoleName, roleName)
@@ -65,7 +68,8 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
     }
 
     @Override
-    public int updateUser(RolePojo rolePojo) {
+    @Transactional
+    public int updateUser(final RolePojo rolePojo) {
         final int exists = this.baseMapper.selectCount(new QueryWrapper<Role>().lambda()
                 .eq(Role::getRoleName, rolePojo.getRoleName())
                 .eq(Role::getDeleted, UserConstants.USER_NOT_DELETED)
@@ -80,7 +84,8 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
     }
 
     @Override
-    public boolean deleteRole(String roleId) {
+    @Transactional
+    public boolean deleteRole(final String roleId) {
         Role delRole = this.baseMapper.selectOne(new QueryWrapper<Role>().lambda().eq(Role::getRoleId, roleId));
         Assert.notNull(delRole, "该角色不存在！");
         delRole.setDeleted(UserConstants.USER_DELETED);
@@ -88,7 +93,8 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
     }
 
     @Override
-    public boolean deleteUserBatch(List<String> roleIds) throws Exception{
+    @Transactional
+    public boolean deleteUserBatch(final List<String> roleIds) throws Exception{
         for(String roleId : roleIds){
             final boolean result = deleteRole(roleId);
             if(!result){
@@ -100,7 +106,25 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
 
     @Override
     public RoleMenuPojo listMenu(final String roleId) {
-
+        List<MenuPojo> menuList = this.baseMapper.listMenuByRoleId(roleId);
+        RoleMenuPojo roleMenuPojo = new RoleMenuPojo();
+        roleMenuPojo.setRoleId(roleId);
+        roleMenuPojo.setMenuList(menuList);
         return null;
+    }
+
+    @Override
+    @Transactional
+    public boolean bindMenu(final RoleMenuPojo roleMenuPojo) {
+        final String roleId = roleMenuPojo.getRoleId();
+        roleMenuService.remove(new QueryWrapper<RoleMenu>().lambda().eq(RoleMenu::getRoleId, roleId));
+        List<RoleMenu> roleMenuList = new ArrayList<>();
+        roleMenuPojo.getMenuList().stream().forEach(menuPojo -> {
+            RoleMenu roleMenu = new RoleMenu();
+            roleMenu.setRoleId(roleId);
+            roleMenu.setMenuId(menuPojo.getMenuId());
+            roleMenuList.add(roleMenu);
+        });
+        return roleMenuService.saveBatch(roleMenuList);
     }
 }
---
+++ src/main/java/com/xh/microservice/user_service/RoleMenuService.java
@@ -0,0 +1,12 @@
+package com.xh.microservice.user_service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xh.microservice.user_entity.RoleMenu;
+
+/**
+ * @Author: x18266
+ * @Description:
+ * @Date: Created in 18:41 2020/10/07
+ */
+public interface RoleMenuService extends IService<RoleMenu> {
+}
